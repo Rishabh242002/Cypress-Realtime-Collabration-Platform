1d8887a9db38103fec2f41be38492cec37f91a0b
Aniket Pathak <aniketpathak@Anikets-MacBook-Air.local>
2024-02-12 17:00:33 +0530

    INIT

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+// @ts-check
+
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+  images: {
+    domains: ["spvbityzljitkpeqigau.supabase.co"],
+  },
+};
 
 export default nextConfig;
